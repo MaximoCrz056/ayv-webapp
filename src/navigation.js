612c3c86e0522c577e911605b0601fc132827e32
MaximoCrz056 <maximogomezcruz@gmail.com>
2024-07-07 19:09:37 -0600

    initial core n pages

---
+++ src/navigation.js
@@ -0,0 +1,79 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'Inicio',
+      href: getPermalink('/'),
+    },
+    {
+      text: 'Activate!',
+      links: [
+        {
+          text: 'Caracteristicas (Inicio)',
+          href: getPermalink('/#features'),
+        },
+        {
+          text: 'Videos',
+          href: getPermalink('/videos'),
+        },
+        {
+          text: 'Dietas',
+          href: getPermalink('/pricing'),
+        },
+      ],
+    },
+    {
+      text: 'Sobre nosotros',
+      href: getPermalink('/about'),
+    },
+    {
+      text: 'Blog',
+      links: [
+        {
+          text: 'Blog List',
+          href: getBlogPermalink(),
+        },
+        {
+          text: 'Article',
+          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+        },
+        {
+          text: 'Article (with MDX)',
+          href: getPermalink('markdown-elements-demo-post', 'post'),
+        },
+        {
+          text: 'Category Page',
+          href: getPermalink('tutorials', 'category'),
+        },
+        {
+          text: 'Tag Page',
+          href: getPermalink('astro', 'tag'),
+        },
+      ],
+    },
+    {
+      text: 'Contacto',
+      href: '#',
+    },
+  ],
+  actions: [{ text: 'Iniciar sesión',icon: 'tabler:login', href: '/iniciar-sesion', target: '_blank' }],
+};
+
+export const footerData = {
+  secondaryLinks: [
+    { text: 'Terms', href: getPermalink('/terms') },
+    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+  ],
+  socialLinks: [
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/MaximoCrz056',},
+  ],
+  footNote: `
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
+    Hecho por <a class="text-blue-600 underline dark:text-muted" href="/ByteBusters"> ByteBusters</a> · Todos los derechos reservados.
+  `,
+};
